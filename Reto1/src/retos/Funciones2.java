993e9488ba1e64ec38e7b48aaa217b3f2d3100b2
pilar <piruletaabad2002@gmail.com>
2024-10-29 09:33:47 +0100

    Añadidas funciones esPar, esImpar, esPositivo y esNegativo

--- Reto1/src/retos/Funciones2.java
+++ Reto1/src/retos/Funciones2.java
@@ -2,4 +2,33 @@ package retos;
 
 public class Funciones2 {
 
+	public static boolean esPar (int n) {
+		if (n % 2 == 0) 
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean esImpar (int n) {
+		if (n % 2 == 0) 
+			return false;
+		else
+			return true;
+	}
+	
+	public static boolean esPositivo (int n) {
+		if (n > 0) 
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean esNegativo (int n) {
+		if (n < 0) 
+			return true;
+		else
+			return false;
+	}
+	
+	
 }
