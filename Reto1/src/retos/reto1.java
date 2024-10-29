38f4d066222071d1c97ac5f7b733d00810580bdb
pilar <piruletaabad2002@gmail.com>
2024-10-29 09:57:56 +0100

    Añadido menú de opciones

--- Reto1/src/retos/reto1.java
+++ Reto1/src/retos/reto1.java
@@ -1,10 +1,50 @@
 package retos;
 
+import java.util.Scanner;
+
 public class reto1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		int operacion = -1;
+		boolean mal;
+		
+		do {
+			mal = false;
+			System.out.println("Elige la operación:");
+			System.out.println("1. Sumar");
+			System.out.println("2. Restar");
+			System.out.println("3. Multiplicar");
+			System.out.println("4. Dividir");
+			System.out.println("5. Par");
+			System.out.println("6. Impar");
+			System.out.println("7. Positivo");
+			System.out.println("8. Negativo");
+			System.out.println("0. Salir");
+			
+			try {
+				String s = sc.nextLine();
+				operacion = Integer.parseInt(s);
+				if (operacion < 0 || operacion > 8) {
+					mal = true;
+					System.out.println("Entre 0 y 8");
+				}
+			} catch (Exception e) {
+				System.out.println("Formato inválido");
+				mal = true;
+			}
+			
+			if (!mal) {
+				
+				
+				
+			}
+				
+			
+		} while (operacion != 0);
+		
+		sc.close();
 	}
 
 }
