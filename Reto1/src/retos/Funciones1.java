d9a9a0c8d191a42f8a95300f36bb330885306b14
mariopellicer <marioopelli23@gmail.com>
2024-10-29 09:47:01 +0100

    Añadidas funciones sumar,restar,esPar,esImpar

--- Reto1/src/retos/Funciones1.java
+++ Reto1/src/retos/Funciones1.java
@@ -1,5 +1,26 @@
 package retos;
 
 public class Funciones1 {
-
+	public static int sumar(int a, int b) {
+		int suma = a + b;
+		return suma;
+	}
+	public static int restar(int a,int b) {
+		int resta = a-b;
+		return resta;
+	}
+	public static boolean esPar(int n) {
+		if (n%2==0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public static boolean esImpar(int n) {
+		if (n%2!=0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
